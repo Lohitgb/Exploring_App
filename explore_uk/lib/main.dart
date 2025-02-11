3313f3654c292c28c7726b71913d33e4335743b1
lohit_gb <lohitbhandari442@gmail.com>
2025-02-11 16:53:41 +0530

    carousal and bottom navigation bar created

--- explore_uk/lib/main.dart
+++ explore_uk/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:explore_uk/pages/forgot.dart';
+import 'package:explore_uk/pages/home.dart';
 import 'package:explore_uk/pages/landing_page.dart';
-import 'package:explore_uk/pages/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -8,15 +7,14 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      // options: const FirebaseOptions(
-      //     apiKey: "AIzaSyAwLhmwAGbYlecz9dcd9Bl4i1q4ZvqdHlo",
-      //     authDomain: "exploreuk-5dea7.firebaseapp.com",
-      //     projectId: "exploreuk-5dea7",
-      //     storageBucket: "exploreuk-5dea7.firebasestorage.app",
-      //     messagingSenderId: "624959196075",
-      //     appId: "1:624959196075:web:846e95f1d74da18eeaa0ee",
-      //     measurementId: "G-TGB787JDLB"
-      );
+      options: const FirebaseOptions(
+          apiKey: "AIzaSyAwLhmwAGbYlecz9dcd9Bl4i1q4ZvqdHlo",
+          authDomain: "exploreuk-5dea7.firebaseapp.com",
+          projectId: "exploreuk-5dea7",
+          storageBucket: "exploreuk-5dea7.firebasestorage.app",
+          messagingSenderId: "624959196075",
+          appId: "1:624959196075:web:846e95f1d74da18eeaa0ee",
+          measurementId: "G-TGB787JDLB"));
   runApp(MyApp());
 }
 
--- explore_uk/lib/pages/home.dart
+++ explore_uk/lib/pages/home.dart
@@ -1,6 +1,8 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:explore_uk/pages/signin.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
@@ -24,36 +26,94 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  int selectedIndex = 0;
+
+  void onItemTapped(int index) {
+    if (index == 3) {
+      signout();
+    } else {
+      setState(() {
+        selectedIndex = index;
+      });
+    }
+  }
+
+  Widget _carousalImage(String imagePath) {
+    return Container(
+      margin: EdgeInsets.all(6.0),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(6.0),
+          image:
+              DecorationImage(image: AssetImage(imagePath), fit: BoxFit.cover)),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: const Text('Homepage'),
-        backgroundColor: Colors.green[200],
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            LottieBuilder.network(
-              // 'https://lottie.host/6f5172ff-6d22-4647-96e1-d6b0ebe744ac/EsUbMGwwJw.json',
-              'https://lottie.host/e220d3a3-1cc3-4e6f-9dfc-25f8bc335bfb/Q7SxW0uYkA.json',
-              repeat: true,
-              animate: true,
-            ),
-            Text(
-              'Work in Progress',
-              style: GoogleFonts.getFont('Roboto Condensed',
-                  fontSize: 25, fontWeight: FontWeight.bold),
-            )
-          ],
+        centerTitle: false,
+        backgroundColor: Color(0xFF00BF6D),
+        foregroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Explore Places',
+          style: GoogleFonts.getFont('Roboto Condensed',
+              fontSize: 22, fontWeight: FontWeight.bold),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: signout,
-        child: Icon(Icons.exit_to_app),
+      body: Column(children: [
+        Container(
+          padding: EdgeInsets.fromLTRB(16, 0, 16, 16),
+          color: Color(0xFF00BF6D),
+          child: Form(
+              child: TextFormField(
+            autofocus: true,
+            textInputAction: TextInputAction.search,
+            onChanged: (value) {},
+            decoration: InputDecoration(
+              hintText: 'Search',
+              hintStyle: TextStyle(
+                // ignore: deprecated_member_use
+                color: Color(0xFF1D1D35).withOpacity(0.64),
+              ),
+              filled: true,
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 16.0 * 1.5, vertical: 12.0),
+              border: OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                  borderRadius: BorderRadius.circular(50.0)),
+            ),
+          )),
+        ),
+        SizedBox(height: 5),
+        Column(
+          children: [
+            CarouselSlider(
+                items: [
+                  _carousalImage('assets/images/yana-745780.jpg'),
+                  _carousalImage('assets/images/magod.jpg'),
+                  _carousalImage('assets/images/sathodi1.jpg'),
+                  _carousalImage('assets/images/forest.jpg')
+                ],
+                options: CarouselOptions(
+                    height: 180.0, enlargeCenterPage: true, autoPlay: true))
+          ],
+        )
+      ]),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'home'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'profile'),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'map'),
+          BottomNavigationBarItem(icon: Icon(Icons.logout), label: 'logout'),
+        ],
+        backgroundColor: Color(0xFF00BF6D),
+        type: BottomNavigationBarType.fixed,
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.white,
+        onTap: onItemTapped,
       ),
     );
   }
